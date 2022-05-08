e2622d92971c2c6163f4c7e24b34c465bbb4e444
eavm <enriqueadhemar@gmail.com>
2022-05-08 16:36:03 -0400

    dia 3 :D

---
+++ projectMain.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+/**
+Menu
+**/
+void menu()
+{
+    cout << "El menu funciona" << endl;
+}
+
+void opcion1()
+{
+    // 1. Añadir nueva ruta
+}
+void opcion2()
+{
+    // 2. elegir punto de partida y destino
+}
+void opcion3()
+{
+    // 3. Viaje rapido
+}
+void opcion4()
+{
+    // 4. Viaje Económico
+}
+
+int main()
+{
+    menu();
+
+    return 0;
+}
