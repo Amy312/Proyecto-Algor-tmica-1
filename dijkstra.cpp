37a8515ebbef9366a6bdd0b0eab3214a18c7380f
Amy312 <ambarrojasm@gmail.com>
2022-05-08 18:41:29 -0400

    :D

---
+++ dijkstra.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<pair <int,int> > grafoM[100000], grafoT[100000];
+map<string, int> Lugar;
+int main(){
+    int aris, vertices, costo, tiempo;
+    string a, b;
+    cin >> aris >> vertices;
+    for(int i=0; i<vertices; i++){
+        cin >> a>> b>> costo >> tiempo;
+        
+
+    }
+    
+}
