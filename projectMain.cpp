eec588e46bc4557bb2730a2af560c68a78deb6c0
Amy312 <ambarrojasm@gmail.com>
2022-05-08 17:00:16 -0400

    menu :D

--- projectMain.cpp
+++ projectMain.cpp
@@ -6,7 +6,28 @@ Menu
 **/
 void menu()
 {
-    cout << "El menu funciona" << endl;
+    int opcion;
+    cout << "   El menu funciona" << endl;
+    cout << "---_---_---_---_---_---_" << endl;
+    cout << "1. Añadir nueva ruta" << endl;
+    cout << "2. Elegir punto de partida y destino" << endl;
+    cout << "3. Viaje Rápido" << endl;
+    cout << "4. Viaje Económico" << endl;
+    cin >> opcion;
+    switch (opcion)
+    {
+    case 1:
+        opcion1();
+    case 2:
+        opcion2();
+    case 3:
+        opcion3();
+    case 4:
+        opcion4();
+    default:
+        cout << ":c Intente de nuevo" << endl;
+        menu();
+    }
 }
 
 void opcion1()
